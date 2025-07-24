62469d03866af4723a7a14f07e561bcc1644b34e
Ben Chapuma <bece20-bchapuma@mubas.ac.mw>
2025-07-24 02:19:53 -0700

    "Nineth commit; no major changes"

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useDebouncedCallback } from 'use-debounce';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
@@ -10,7 +11,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   
   function handleSearch(term: string) {
     console.log(`Searching... ${term}`);
-    
+
     const params = new URLSearchParams(searchParams);
     if (term) {
       params.set('query', term);
